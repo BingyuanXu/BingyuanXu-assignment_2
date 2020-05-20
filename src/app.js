0208d779ed9d3e17dc910b1c78d3f1478c82c4bf
BingyuanXu <xubingyuan123@hotmail.com>
2020-05-20 17:57:07 -0500

    render current weather card

--- src/app.js
+++ src/app.js
@@ -1,32 +1,39 @@
-let apiKey = `b8aa8c39c4012f71e59cf320be595b6b`;
-
-//Current Weather Data  
-//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
-// api.openweathermap.org/data/2.5/weather?lat=35&lon=139
-
-// icon  https://openweathermap.org/weather-conditions
-// http://openweathermap.org/img/wn/10d@2x.png 
+const apiKey = `b8aa8c39c4012f71e59cf320be595b6b`;
 
 navigator.geolocation.getCurrentPosition(success, error);
 
 function success(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
+
   getCurrentWeatherJson(latitude, longitude)
+    .then((json) => renderCurrentWeather(json));
 }
 
 function error() {
-  status.textContent = 'Unable to retrieve your location';
+  status.textContent = "Unable to retrieve your location";
 }
 
 function getCurrentWeatherJson(latitude, longitude) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`)
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`)
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
         throw new Error(`There is a problem in current weather`);
       }
-    })
-    .then((json) => console.log(json));
+    });
+}
+
+function renderCurrentWeather(json) {
+  const currentConditionsEle = document.querySelector(`.current-conditions`);
+  
+  currentConditionsEle.innerHTML = `
+     <h2>Current Conditions</h2>
+     <img src="http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png" />
+     <div class="current">
+     <div class="temp">${Math.round(json.main.temp)}℃</div>
+     <div class="condition">${json.weather[0].description}</div>
+    `;
 }
